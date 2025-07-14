2e364f4daad93a965e530cfe084cc5d28ec819d5
deslrey <2135232170@qq.com>
2025-07-14 23:28:05 +0800

    feat: go语言的输出

---
+++ demo01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("你好golang")
+	fmt.Println("你好golang")
+
+	fmt.Print("aaa")
+	fmt.Print("bbb")
+	fmt.Print("ccc")
+
+	fmt.Println("aaa")
+
+	fmt.Println("bbb")
+	fmt.Println("ccc")
+
+	fmt.Println("go", "python", "php", "javascript")
+	fmt.Print("go", "python", "php", "javascript")
+
+}
